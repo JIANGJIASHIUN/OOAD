11f019f95403620abbd69aaa2115778910928189
yoyo835566 <yoyo835566@gmail.com>
2016-04-24 16:02:08 +0800

    Login Interface

---
+++ OOAD-HR System/OOAD-HR System/Login.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace OOAD_HR_System
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.employeeID = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(640, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Human Resource System!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(110, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(110, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // employeeID
+            // 
+            this.employeeID.Location = new System.Drawing.Point(276, 147);
+            this.employeeID.Name = "employeeID";
+            this.employeeID.Size = new System.Drawing.Size(282, 29);
+            this.employeeID.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(236, 219);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(322, 29);
+            this.password.TabIndex = 4;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.loginButton.Location = new System.Drawing.Point(454, 314);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(104, 35);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 379);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.employeeID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Human Resource System Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox employeeID;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
