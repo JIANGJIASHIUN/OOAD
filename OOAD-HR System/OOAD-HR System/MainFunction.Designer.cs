3df5a049a726020c848788215f52a79586d1bc9a
yoyo835566 <yoyo835566@gmail.com>
2016-04-24 16:22:29 +0800

    MainFunction version1 only tag page

---
+++ OOAD-HR System/OOAD-HR System/MainFunction.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace OOAD_HR_System
+{
+    partial class MainFunction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainFunctionPage = new System.Windows.Forms.TabControl();
+            this.employee = new System.Windows.Forms.TabPage();
+            this.workAttendance = new System.Windows.Forms.TabPage();
+            this.reimburse = new System.Windows.Forms.TabPage();
+            this.bonus = new System.Windows.Forms.TabPage();
+            this.salary = new System.Windows.Forms.TabPage();
+            this.traning = new System.Windows.Forms.TabPage();
+            this.careerPlanning = new System.Windows.Forms.TabPage();
+            this.report = new System.Windows.Forms.TabPage();
+            this.authorization = new System.Windows.Forms.TabPage();
+            this.mainFunctionPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainFunctionPage
+            // 
+            this.mainFunctionPage.Controls.Add(this.employee);
+            this.mainFunctionPage.Controls.Add(this.workAttendance);
+            this.mainFunctionPage.Controls.Add(this.reimburse);
+            this.mainFunctionPage.Controls.Add(this.bonus);
+            this.mainFunctionPage.Controls.Add(this.salary);
+            this.mainFunctionPage.Controls.Add(this.traning);
+            this.mainFunctionPage.Controls.Add(this.careerPlanning);
+            this.mainFunctionPage.Controls.Add(this.report);
+            this.mainFunctionPage.Controls.Add(this.authorization);
+            this.mainFunctionPage.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.mainFunctionPage.Location = new System.Drawing.Point(12, 12);
+            this.mainFunctionPage.Name = "mainFunctionPage";
+            this.mainFunctionPage.SelectedIndex = 0;
+            this.mainFunctionPage.Size = new System.Drawing.Size(1566, 911);
+            this.mainFunctionPage.TabIndex = 0;
+            // 
+            // employee
+            // 
+            this.employee.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.employee.Location = new System.Drawing.Point(4, 34);
+            this.employee.Name = "employee";
+            this.employee.Padding = new System.Windows.Forms.Padding(3);
+            this.employee.Size = new System.Drawing.Size(1558, 873);
+            this.employee.TabIndex = 0;
+            this.employee.Text = "Employee";
+            this.employee.UseVisualStyleBackColor = true;
+            // 
+            // workAttendance
+            // 
+            this.workAttendance.Location = new System.Drawing.Point(4, 28);
+            this.workAttendance.Name = "workAttendance";
+            this.workAttendance.Padding = new System.Windows.Forms.Padding(3);
+            this.workAttendance.Size = new System.Drawing.Size(1558, 879);
+            this.workAttendance.TabIndex = 1;
+            this.workAttendance.Text = "Work Attendance";
+            this.workAttendance.UseVisualStyleBackColor = true;
+            // 
+            // reimburse
+            // 
+            this.reimburse.Location = new System.Drawing.Point(4, 28);
+            this.reimburse.Name = "reimburse";
+            this.reimburse.Padding = new System.Windows.Forms.Padding(3);
+            this.reimburse.Size = new System.Drawing.Size(1558, 879);
+            this.reimburse.TabIndex = 2;
+            this.reimburse.Text = "Reimburse";
+            this.reimburse.UseVisualStyleBackColor = true;
+            // 
+            // bonus
+            // 
+            this.bonus.Location = new System.Drawing.Point(4, 28);
+            this.bonus.Name = "bonus";
+            this.bonus.Padding = new System.Windows.Forms.Padding(3);
+            this.bonus.Size = new System.Drawing.Size(1558, 879);
+            this.bonus.TabIndex = 3;
+            this.bonus.Text = "Bonus";
+            this.bonus.UseVisualStyleBackColor = true;
+            // 
+            // salary
+            // 
+            this.salary.Location = new System.Drawing.Point(4, 28);
+            this.salary.Name = "salary";
+            this.salary.Padding = new System.Windows.Forms.Padding(3);
+            this.salary.Size = new System.Drawing.Size(1558, 879);
+            this.salary.TabIndex = 4;
+            this.salary.Text = "Salary";
+            this.salary.UseVisualStyleBackColor = true;
+            // 
+            // traning
+            // 
+            this.traning.Location = new System.Drawing.Point(4, 28);
+            this.traning.Name = "traning";
+            this.traning.Padding = new System.Windows.Forms.Padding(3);
+            this.traning.Size = new System.Drawing.Size(1558, 879);
+            this.traning.TabIndex = 5;
+            this.traning.Text = "Traning";
+            this.traning.UseVisualStyleBackColor = true;
+            // 
+            // careerPlanning
+            // 
+            this.careerPlanning.Location = new System.Drawing.Point(4, 28);
+            this.careerPlanning.Name = "careerPlanning";
+            this.careerPlanning.Padding = new System.Windows.Forms.Padding(3);
+            this.careerPlanning.Size = new System.Drawing.Size(1558, 879);
+            this.careerPlanning.TabIndex = 6;
+            this.careerPlanning.Text = "Career Plainning";
+            this.careerPlanning.UseVisualStyleBackColor = true;
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(4, 28);
+            this.report.Name = "report";
+            this.report.Padding = new System.Windows.Forms.Padding(3);
+            this.report.Size = new System.Drawing.Size(1558, 879);
+            this.report.TabIndex = 7;
+            this.report.Text = "Report";
+            this.report.UseVisualStyleBackColor = true;
+            // 
+            // authorization
+            // 
+            this.authorization.Location = new System.Drawing.Point(4, 28);
+            this.authorization.Name = "authorization";
+            this.authorization.Padding = new System.Windows.Forms.Padding(3);
+            this.authorization.Size = new System.Drawing.Size(1558, 879);
+            this.authorization.TabIndex = 8;
+            this.authorization.Text = "Authorization";
+            this.authorization.UseVisualStyleBackColor = true;
+            // 
+            // MainFunction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1590, 935);
+            this.Controls.Add(this.mainFunctionPage);
+            this.Name = "MainFunction";
+            this.Text = "MainFunction";
+            this.mainFunctionPage.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl mainFunctionPage;
+        private System.Windows.Forms.TabPage employee;
+        private System.Windows.Forms.TabPage workAttendance;
+        private System.Windows.Forms.TabPage reimburse;
+        private System.Windows.Forms.TabPage bonus;
+        private System.Windows.Forms.TabPage salary;
+        private System.Windows.Forms.TabPage traning;
+        private System.Windows.Forms.TabPage careerPlanning;
+        private System.Windows.Forms.TabPage report;
+        private System.Windows.Forms.TabPage authorization;
+    }
+}
--- OOAD-HR System/OOAD-HR System/MainFunction.cs
+++ OOAD-HR System/OOAD-HR System/MainFunction.cs
@@ -10,18 +10,11 @@ using System.Windows.Forms;
 
 namespace OOAD_HR_System
 {
-    public partial class Login : Form
+    public partial class MainFunction : Form
     {
-        public Login()
+        public MainFunction()
         {
             InitializeComponent();
-        }
-
-        private void loginButton_Click(object sender, EventArgs e)
-        {
-            Form mainFunction = new MainFunction();
-            mainFunction.Show();
-            this.Visible = false;
         }
     }
 }
