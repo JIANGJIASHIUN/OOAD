b99f0ee422eab6b9aa9dafd3a4c4d5ca0e94b6b5
bleach486 <you@example.com>
2016-04-25 16:26:06 +0800

    no message

---
+++ OOAD-HR System/Observer ex/Observer/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+    public partial class Form1 : Form
+    {
+        ObserverDemo _observer;
+        public Form1()
+        {
+            InitializeComponent();
+            _observer = new ObserverDemo();
+            _observer.labelHandler += RefreshBtnContent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _observer.ClickDemoBtn();
+        }
+
+        private void RefreshBtnContent()
+        {
+            label1.Text = "Observer";
+        }
+    }
+}
