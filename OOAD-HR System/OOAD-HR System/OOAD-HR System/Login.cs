97d41162b597f7ea0445c2d8fde5339b805b463c
yoyo835566 <yoyo835566@gmail.com>
2016-05-05 17:13:12 +0900

    add class v5

---
+++ OOAD-HR System/OOAD-HR System/OOAD-HR System/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOAD_HR_System
+{
+    public partial class _login : Form
+    {
+
+        private Form _mainFunction;
+
+        public _login()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickLoginButton(object sender, EventArgs e)
+        {
+            _mainFunction = new _mainFunction(this);
+            _mainFunction.Show();
+            this.Visible = false;
+        }
+    }
+}
